91e87598dc98c46aa27de92f69e80b45ed8585f7
KENNYRock369 <hectortrinidad113@gmail.com>
2024-07-09 22:23:48 -0600

    Cambios en login y registro xd

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,9 @@ import usuarioImg from '../img/usuario.png';
 import { NavLink, useHistory } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
+import '../Styles/Login.css'
+import { Button } from "@nextui-org/react";
+
 import {
   IonButton,
   IonCard,
@@ -52,39 +55,57 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   return (
     <IonContent>
-      <IonCard style={{ maxWidth: '400px', margin: '0 auto', marginTop: '50px', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-        <img alt="Silueta de montañas" src={usuarioImg}  style={{ width: '40%', borderRadius: '10px', marginRight: '105px', marginLeft: 'auto', display:'block' }} />
+      <IonCard style={{ maxWidth: '400px', margin: '0 auto', marginTop: '10px', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
+        <img alt="Silueta de montañas" src={usuarioImg} style={{ width: '40%', borderRadius: '10px', marginRight: '105px', marginLeft: 'auto', display: 'block' }} />
         <IonCardHeader style={{ textAlign: 'center' }}>
           <IonCardTitle style={{ fontSize: '24px', fontWeight: 'bold' }}>Iniciar Sesión</IonCardTitle>
           <IonCardSubtitle style={{ color: '#666' }}>Inicia sesión para entrar al sistema</IonCardSubtitle>
         </IonCardHeader>
 
-        <form onSubmit={handleLogin} style={{ marginTop: '5px' }}>
+        <form onSubmit={handleLogin} style={{ marginTop: '50px' }}>
           <IonList>
-            <IonItem>
+            <IonItem lines="none" style={{ padding: '10px 0'}}>
               <IonInput
                 type="email"
                 value={email}
                 onIonChange={(e) => setEmail(e.detail.value!)}
                 required
-                placeholder="Dirección de correo electrónico"
-                style={{ width: '100%', padding: '10px , 10em', borderRadius: '6px', border: '1px solid ',boxSizing: 'border-box' }}
+                placeholder="  Email"
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  borderRadius: '6px',
+                  border: 'none', // Añade un borde sólido para mayor visibilidad
+                  boxSizing: 'border-box', // Asegura que el padding no incremente el ancho total
+                  background: '#eaeaea',
+                  color: ' #212121 '
+                }}
               />
             </IonItem>
-            <IonItem>
+
+            <IonItem lines="none" style={{ padding: '10px 0' }}>
               <IonInput
                 type="password"
                 value={password}
                 onIonChange={(e) => setPassword(e.detail.value!)}
                 required
-                placeholder="Contraseña"
-                style={{ width: '100%', padding: '10px, 10em', borderRadius: '6px', border: '1px solid ',boxSizing: 'border-box' }}
+                placeholder="  Contraseña"
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  borderRadius: '6px',
+                  border: 'none', // Añade un borde sólido para mayor visibilidad
+                  boxSizing: 'border-box', // Asegura que el padding no incremente el ancho total
+                  background: '#eaeaea',
+                  color: 'black' // Asegura que el padding no incremente el ancho total
+                }}
               />
             </IonItem>
+
           </IonList>
-          <IonButton expand="full" type="submit" disabled={loading} style={{ width: '100%', marginTop: '20px', borderRadius: '4px', backgroundColor: '#007bff', color: '#fff', border: 'none' }}>
+          <Button type="submit" disabled={loading} style={{ width: '100%', marginTop: '20px', backgroundColor: '#007bff', color: '#fff' }}>
             {loading ? <IonSpinner name="crescent" /> : 'Iniciar sesión'}
-          </IonButton>
+          </Button>
         </form>
         {errorMessage && <p style={{ color: 'red', textAlign: 'center', marginTop: '20px' }}>{errorMessage}</p>}
         <p style={{ textAlign: 'center', marginTop: '20px' }}>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,18 +3,20 @@ import Register1Img from '../img/nuevo.png';
 import { NavLink, useHistory } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
-import { 
-    IonContent, 
-    IonList, 
-    IonItem, 
-    IonButton, 
-    IonInput, 
-    IonCard, 
-    IonCardHeader, 
-    IonCardTitle, 
-    IonCardSubtitle, 
-    IonSpinner, 
-    IonAlert 
+import { Button } from "@nextui-org/react";
+
+import {
+    IonContent,
+    IonList,
+    IonItem,
+    IonButton,
+    IonInput,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonSpinner,
+    IonAlert
 } from '@ionic/react';
 
 const Registro: React.FC = () => {
@@ -48,43 +50,59 @@ const Registro: React.FC = () => {
 
     return (
         <IonContent>
-      <IonCard style={{ maxWidth: '400px', margin: '0 auto', marginTop: '50px', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-                <img alt="Silueta de montañas" src={Register1Img}  style={{ width: '40%', borderRadius: '10px', marginRight: '105px', marginLeft: 'auto', display:'block' }}   />
-         <IonCardHeader style={{ textAlign: 'center' }}>
-          <IonCardTitle style={{ fontSize: '24px', fontWeight: 'bold' }}>Registrarse</IonCardTitle>
-          <IonCardSubtitle style={{ color: '#666' }}>Registrarse para entrar al sistema</IonCardSubtitle>
+            <IonCard style={{ maxWidth: '400px', margin: '0 auto', marginTop: '10px', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
+                <img alt="Silueta de montañas" src={Register1Img} style={{ width: '40%', borderRadius: '10px', marginRight: '105px', marginLeft: 'auto', display: 'block' }} />
+                <IonCardHeader style={{ textAlign: 'center' }}>
+                    <IonCardTitle style={{ fontSize: '24px', fontWeight: 'bold' }}>Registrarse</IonCardTitle>
+                    <IonCardSubtitle style={{ color: '#666' }}>Registrarse para entrar al sistema</IonCardSubtitle>
                 </IonCardHeader>
                 <IonList>
                     <form onSubmit={onSubmit} style={{ marginTop: '5px' }}>
-                        <IonItem>
-                            <IonInput
-                                type="email"
-                                value={email}
-                                onIonChange={(e) => setEmail(e.detail.value!)}
-                                required
-                                placeholder="Dirección de correo electrónico"
-                                style={{ width: '100%', padding: '10px , 10em', borderRadius: '6px', border: '1px solid ',boxSizing: 'border-box' }}
-                                />
-                        </IonItem>
-                        <IonItem>
-                            <IonInput
-                                type="password"
-                                value={contraseña}
-                                onIonChange={(e) => setContraseña(e.detail.value!)}
-                                required
-                                placeholder="Contraseña"
-                                style={{ width: '100%', padding: '10px , 10em', borderRadius: '6px', border: '1px solid ',boxSizing: 'border-box' }}
-                                />
-                        </IonItem>
-                        <IonButton expand="full" type="submit" disabled={loading} style={{ width: '100%', marginTop: '20px', borderRadius: '4px', backgroundColor: '#007bff', color: '#fff', border: 'none' }}>
+                        <IonList>
+                            <IonItem lines="none" style={{ padding: '10px 0' }}>
+                                <IonInput
+                                    type="email"
+                                    value={email}
+                                    onIonChange={(e) => setEmail(e.detail.value!)}
+                                    required
+                                    placeholder="  Email"
+                                    style={{
+                                        width: '100%',
+                                        padding: '10px',
+                                        borderRadius: '6px',
+                                        border: 'none', // Añade un borde sólido para mayor visibilidad
+                                        boxSizing: 'border-box', // Asegura que el padding no incremente el ancho total
+                                        background: '#eaeaea',
+                                        color: ' #212121 ' // Asegura que el padding no incremente el ancho total
+                                      }}/>
+                            </IonItem>
+                            <IonItem lines="none" style={{ padding: '10px 0' }}>
+                                <IonInput
+                                    type="password"
+                                    value={contraseña}
+                                    onIonChange={(e) => setContraseña(e.detail.value!)}
+                                    required
+                                    placeholder="  Contraseña"
+                                    style={{
+                                        width: '100%',
+                                        padding: '10px',
+                                        borderRadius: '6px',
+                                        border: 'none', // Añade un borde sólido para mayor visibilidad
+                                        boxSizing: 'border-box', // Asegura que el padding no incremente el ancho total
+                                        background: '#eaeaea',
+                                        color: 'black' // Asegura que el padding no incremente el ancho total
+                                      }} />
+                            </IonItem>
+                        </IonList>
+                        <Button type="submit" disabled={loading} style={{ width: '100%', marginTop: '20px', backgroundColor: '#007bff', color: '#fff' }}>
                             {loading ? <IonSpinner /> : 'Registrarse'}
-                        </IonButton>
+                        </Button>
                     </form>
                     {errorMessage && <p style={{ color: 'red', textAlign: 'center', marginTop: '20px' }}>{errorMessage}</p>}
                 </IonList>
                 <p style={{ textAlign: 'center', marginTop: '20px' }}>
                     ¿Ya tienes una cuenta?{' '}
-                    <NavLink to="/login"style={{ color: '#007bff', textDecoration: 'none' }}>Iniciar sesión</NavLink>
+                    <NavLink to="/login" style={{ color: '#007bff', textDecoration: 'none' }}>Iniciar sesión</NavLink>
                 </p>
             </IonCard>
             <IonAlert
